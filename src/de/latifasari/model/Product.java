290781e809d32bc111a8e255b27003360b036c7e
dvckbass <duckbass@live.com>
2021-01-22 20:12:59 +0100

    Modelle werden hinzugefügt

---
+++ src/de/latifasari/model/Product.java
@@ -0,0 +1,70 @@
+package de.latifasari.model;
+
+/**
+ * Das Modell für Product
+ */
+public class Product {
+    private long id;
+    private String name;
+    private String imgFile;
+    private String description;
+    private double price;
+    private int rating;
+
+    public Product(long id, String name, String imgFile, String description, double price, int rating) {
+        this.id = id;
+        this.name = name;
+        this.imgFile = imgFile;
+        this.description = description;
+        this.price = price;
+        this.rating = rating;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+        public String getImgFile() {
+        return imgFile;
+    }
+
+    public void setImgFile(String imgFile) {
+        this.imgFile = imgFile;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+}
---
+++ src/de/latifasari/model/ShoppingCart.java
@@ -0,0 +1,59 @@
+package de.latifasari.model;
+
+import java.util.List;
+
+public class ShoppingCart {
+    private long id;
+    private float discount_absolut;
+    private float discount_percent;
+    private List<Product> products;
+    private int totalProduct;
+
+    public ShoppingCart(long id, float discount_absolut, float discount_percent, List<Product> products, int totalProduct) {
+        this.id = id;
+        this.discount_absolut = discount_absolut;
+        this.discount_percent = discount_percent;
+        this.products = products;
+        this.totalProduct = totalProduct;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public float getDiscount_absolut() {
+        return discount_absolut;
+    }
+
+    public void setDiscount_absolut(float discount_absolut) {
+        this.discount_absolut = discount_absolut;
+    }
+
+    public float getDiscount_percent() {
+        return discount_percent;
+    }
+
+    public void setDiscount_percent(float discount_percent) {
+        this.discount_percent = discount_percent;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    public int getTotalProduct() {
+        return totalProduct;
+    }
+
+    public void setTotalProduct(int totalProduct) {
+        this.totalProduct = totalProduct;
+    }
+}
